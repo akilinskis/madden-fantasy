5479badf8eaf5a3d3b413323da2bf11302929a7c
Andrew <akilins2@illinois.edu>
2020-06-25 22:02:38 -0500

    Test POST endpoint with log

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/test-post', function(req, res) {
+  console.log(req.body);
+  res.status(200);
+});
+
 module.exports = router;
